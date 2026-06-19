d88160f9ff69542ea7eb5d1a35d09c01551c11e4
ganesh <ganeshmuthupandian96@gmail.com>
2026-06-19 19:19:38 +0530

    update

--- script.js
+++ script.js
@@ -2,3 +2,7 @@
 function showMessage(){
     alert("Thank you for visiting my portfolio!");
 }
+function aboutmyself()
+{
+    alert("I'm a Computer Science graduate passionate about Web Development.\nCurrently learning HTML, CSS, JavaScript, React.");
+}
